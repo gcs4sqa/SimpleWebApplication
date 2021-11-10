2d4013d52b245aa10847a6508bba084c5270008a
cliff <cliffordjdouglas@gmail.com>
2021-11-10 09:59:45 +0000

    Initial commit of all files

---
+++ src/main/java/com/gcs4sqa/simplewebapplicaton/SimpleWebApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.gcs4sqa.simplewebapplicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplicatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplicatonApplication.class, args);
+    }
+
+}
